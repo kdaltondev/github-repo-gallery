c765392632d53ae5576ce8407920b3e592d089c5
dalton_k <longlivebicycles@gmail.com>
2023-06-18 19:53:47 -0400

    Added functionality to display specific repo info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const overview = document.querySelector(".overview");
 const gitUserName = "kdaltondev";
 const repoList = document.querySelector(".repo-list");
+const repos = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
 
 const getUserInfo = async function () {
   const info = await fetch(`https://api.github.com/users/${gitUserName}`);
@@ -31,22 +33,70 @@ displayUserInfo = function (data) {
   overview.append(userInfo);
 };
 
-const getRepoList = async function(){
-    console.log("Get repo list");
-    const repoInfo = await fetch(`https://api.github.com/users/${gitUserName}/repos?sort=update&per_page=100`);
+const getRepoList = async function () {
+  console.log("Get repo list");
+  const repoInfo = await fetch(
+    `https://api.github.com/users/${gitUserName}/repos?sort=update&per_page=100`
+  );
   const repoData = await repoInfo.json();
   console.log(repoData);
   displayRepoInfo(repoData);
-}
+};
 
-displayRepoInfo = function (repoData){
-    for (let i=0; i<repoData.length; i++){
-        console.log(`${repoData[i].name}`)
-        const li = document.createElement("li");
-        li.innerHTML = `<h3 class="repo">${repoData[i].name}<h3>`;
-        repoList.append(li);
-        console.log(repoList);
+displayRepoInfo = function (repoData) {
+  for (let i = 0; i < repoData.length; i++) {
+    console.log(`${repoData[i].name}`);
+    const li = document.createElement("li");
+    li.innerHTML = `<h3 class="repo">${repoData[i].name}<h3>`;
+    repoList.append(li);
+    console.log(repoList);
+  }
+};
 
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    console.log(repoName);
+    getSpecificRepoInfo(repoName);
+  } else {
+    console.log("This is it");
+  }
+});
 
-    }
-}
+getSpecificRepoInfo = async function (repoName) {
+  const specificRepoInfo = await fetch(
+    `https://api.github.com/repos/${gitUserName}/${repoName}`
+  );
+  const specificRepoData = await specificRepoInfo.json();
+  console.log(specificRepoData);
+  const fetchLanguages = await fetch(
+    `https://api.github.com/repos/${gitUserName}/${repoName}/languages`
+  );
+  const languageData = await fetchLanguages.json();
+  let languages = [];
+  for (let key in languageData) {
+    languages.push(key);
+  }
+  console.log(languages);
+  displaySpecificRepoInfo(specificRepoData, languages);
+  /*for (i=0; i<specificRepoData.languages.length; i++){
+languages.push(specificRepoData.languages[i]);
+console.log(languages);
+  }*/
+};
+
+displaySpecificRepoInfo = function (repoInfo, languages) {
+  repoData.innerHTML = "";
+  const displayRepoData = document.createElement("div");
+  displayRepoData.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+<p>Description: ${repoInfo.description}</p>
+<p>Default Branch: ${repoInfo.default_branch}</p>
+<p>Languages: ${languages.join(", ")}</p>
+<a class="visit" href="${
+    repoInfo.html_url
+  }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+`;
+  repoData.append(displayRepoData);
+  repoData.classList.remove("hide");
+  repos.classList.add("hide");
+};
