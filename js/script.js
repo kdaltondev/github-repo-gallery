15b09de67a60cc77f3f42b47812a2f5f15ef745b
dalton_k <longlivebicycles@gmail.com>
2023-06-18 17:59:50 -0400

    Added functionality to dipslay github repo list

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 const overview = document.querySelector(".overview");
 const gitUserName = "kdaltondev";
+const repoList = document.querySelector(".repo-list");
 
 const getUserInfo = async function () {
   const info = await fetch(`https://api.github.com/users/${gitUserName}`);
   const data = await info.json();
   console.log(data);
   displayUserInfo(data);
+  /*const repoInfo = await fetch(`https://api.github.com/users/${gitUserName}/repos`);
+  const repoData = await repoInfo.json();
+  console.log(repoData);*/
+  getRepoList();
 };
+
 getUserInfo(gitUserName);
 
 displayUserInfo = function (data) {
@@ -24,3 +30,23 @@ displayUserInfo = function (data) {
     </div>`;
   overview.append(userInfo);
 };
+
+const getRepoList = async function(){
+    console.log("Get repo list");
+    const repoInfo = await fetch(`https://api.github.com/users/${gitUserName}/repos?sort=update&per_page=100`);
+  const repoData = await repoInfo.json();
+  console.log(repoData);
+  displayRepoInfo(repoData);
+}
+
+displayRepoInfo = function (repoData){
+    for (let i=0; i<repoData.length; i++){
+        console.log(`${repoData[i].name}`)
+        const li = document.createElement("li");
+        li.innerHTML = `<h3 class="repo">${repoData[i].name}<h3>`;
+        repoList.append(li);
+        console.log(repoList);
+
+
+    }
+}
